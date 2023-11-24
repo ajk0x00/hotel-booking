55cf8fe70e733a29ba6320c954fbf84786aea521
Abhijith K <abijith.kanjoli@qbusrt.com>
2023-11-24 10:38:04 +0530

    Added spring doc server config

--- src/main/java/com/hotel/api/booking/HotelBookingApplication.java
+++ src/main/java/com/hotel/api/booking/HotelBookingApplication.java
@@ -1,13 +1,20 @@
 package com.hotel.api.booking;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 public class HotelBookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelBookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingApplication.class, args);
+    }
 
 }
