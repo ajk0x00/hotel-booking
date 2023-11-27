49ed492df81685f8e6466691ad8b64e49efe7c4d
Abhijith Kanjoli <abhijith.kanjoli@qburst.com>
2023-11-27 10:16:49 +0000

    Docker

--- src/test/java/com/hotel/api/booking/HotelBookingApplicationTests.java
+++ src/test/java/com/hotel/api/booking/HotelBookingApplicationTests.java
@@ -1,13 +1,8 @@
 package com.hotel.api.booking;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HotelBookingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
